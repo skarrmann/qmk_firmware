4402f20e22960b88756a7a865036b9ae276bc1c8
Steven Karrmann <skarrmann@gmail.com>
2020-01-16 23:32:05 -0500

    Assign layer-tap and mod-tap key mappings for omitted keys.

--- keyboards/preonic/keymaps/skarrmann/keymap.c
+++ keyboards/preonic/keymaps/skarrmann/keymap.c
@@ -17,6 +17,13 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
+// Custom keycode names
+#define LO_LBRC LT(_LOWER, KC_LBRC)
+#define RA_RBRC LT(_RAISE, KC_RBRC)
+#define AS_GRV  LT(_ASIDE, KC_GRV)
+#define RC_EQL  MT(MOD_RCTL, KC_EQL)
+#define RS_BSLS MT(MOD_RSFT, KC_BSLS)
+
 enum preonic_layers {
   _QWERTY,
   _COLEMAK,
@@ -48,15 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RShift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |LCtrl | GUI  | LAlt |Lower |Raise |Space |RCtrl | RAlt | App  | Mouse|Enter |
+ * | Esc  |LCtrl | GUI  | LAlt |Lower |Raise |Space |RCtrl | RAlt | App  |Mouse |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
   KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  ASIDE,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_ESC,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   RAISE,   KC_SPC,  KC_RCTL, KC_RALT, KC_APP,  MOUSE,   KC_ENT   \
+  AS_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RS_BSLS, \
+  KC_ESC,  KC_LCTL, KC_LGUI, KC_LALT, LO_LBRC, RA_RBRC, KC_SPC,  RC_EQL,  KC_RALT, KC_APP,  MOUSE,   KC_ENT   \
 ),
 
 /* Colemak
@@ -186,6 +193,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
         case QWERTY:
@@ -197,34 +208,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case COLEMAK:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_COLEMAK);
-          }
-          return false;
-          break;
-        case LOWER:
-          if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case RAISE:
-          if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case ASIDE:
-          if (record->event.pressed) {
-            layer_on(_ASIDE);
-          } else {
-            layer_off(_ASIDE);
           }
           return false;
           break;
