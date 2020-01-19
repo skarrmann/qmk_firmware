83c28dca947e314469a3cac7b2bb5cf7ce918843
Steven Karrmann <skarrmann@gmail.com>
2020-01-18 19:27:28 -0500

    Rearrange esc, right alt, and mouse keys.

--- keyboards/preonic/keymaps/skarrmann/keymap.c
+++ keyboards/preonic/keymaps/skarrmann/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |LShift|  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /   |RShift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |  [   |  =   |      |  ]   |      |      |      |      |
- * | Esc  |LCtrl | GUI  | LAlt |Lower |RCtrl |Space |Raise | RAlt | App  |Mouse |Enter |
+ * |Mouse |LCtrl | GUI  | LAlt |Lower |RCtrl |Space |Raise | Esc  | App  | RAlt |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   AS_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   LS_CAPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RS_BSLS, \
-  KC_ESC,  KC_LCTL, KC_LGUI, KC_LALT, LO_LBRC, RC_EQL,  KC_SPC,  RA_RBRC,  KC_RALT, KC_APP,  TG_MOUS, KC_ENT   \
+  TG_MOUS, KC_LCTL, KC_LGUI, KC_LALT, LO_LBRC, RC_EQL,  KC_SPC,  RA_RBRC, KC_ESC,  KC_APP,  KC_RALT, KC_ENT   \
 ),
 
 /* Colemak
