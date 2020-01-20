7b5bdc40ad103dd55d8802dd3afb89f7fe327e80
Steven Karrmann <skarrmann@gmail.com>
2020-01-20 07:31:25 -0500

    Re-add lower layer toggle key on left pinky.

--- keyboards/preonic/keymaps/skarrmann/keymap.c
+++ keyboards/preonic/keymaps/skarrmann/keymap.c
@@ -2,8 +2,8 @@
 
 // Custom keycode names
 #define LL_DEL  LT(_LOWER, KC_DEL)
+#define LL_EQL  LT(_LOWER, KC_EQL)
 #define LR_ESC  LT(_RAISE, KC_ESC)
-#define LM_EQL  LT(_MOUSE, KC_EQL)
 #define AL_LBRC MT(MOD_LALT, KC_LBRC)
 #define AR_RBRC MT(MOD_RALT, KC_RBRC)
 #define CL_DEL  MT(MOD_LCTL, KC_DEL)
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Del  |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   |  -   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  =   |      |      |      |      |      |      |      |      |      |      |      |
- * |Mouse |  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |  ;   |  '   |
+ * |Lower |  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |  ;   |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Tab  |      |      |      |      |      |      |      |      |      |      |Enter |
  * |LShift|  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /   |RShift|
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid( \
   KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC, \
   KC_DEL , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS, \
-  LM_EQL , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, \
+  LL_EQL , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, \
   SL_TAB , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SR_ENT , \
   KC_LCTL, KC_LGUI, KC_APP , AL_LBRC, LL_DEL , LR_ESC , KC_SPC , CR_BSPC, AR_RBRC, KC_INS , KC_BSLS, TG_MOUS  \
 ),
