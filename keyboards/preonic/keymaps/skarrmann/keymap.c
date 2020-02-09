bd71bace6a920ce78726e0e3c67708169c7cc532
Steven Karrmann <skarrmann@gmail.com>
2020-02-09 16:55:41 -0500

    Move custom keycode definitions to userspace.

--- keyboards/preonic/keymaps/skarrmann/keymap.c
+++ keyboards/preonic/keymaps/skarrmann/keymap.c
@@ -1,20 +1,5 @@
 #include "skarrmann.h"
 
-// Custom keycode names
-#define OSM_CL  OSM(MOD_LCTL)
-#define CL_DEL  MT(MOD_LCTL, KC_DEL)
-#define CR_BSPC MT(MOD_RCTL, KC_BSPC)
-#define OSM_SL  OSM(MOD_LSFT)
-#define SL_ESC  MT(MOD_LSFT, KC_ESC)
-#define SR_ENT  MT(MOD_RSFT, KC_ENT)
-#define OSL_LL  OSL(_LOWER)
-#define LR_SPC  LT(_RAISE, KC_SPC)
-#define LD_D    LT(_D, KC_D)
-#define LF_F    LT(_F, KC_F)
-#define LJ_J    LT(_J, KC_J)
-#define LK_K    LT(_K, KC_K)
-#define TG_STND TG(_STANDARD)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
