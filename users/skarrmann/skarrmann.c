cc7023dcaafac3b5d25f06ad461b7dacbbc70212
Steven Karrmann <skarrmann@gmail.com>
2020-02-11 22:48:18 -0500

    Only apply longer tapping term to spacebar.

--- users/skarrmann/skarrmann.c
+++ users/skarrmann/skarrmann.c
@@ -5,17 +5,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
-// Indicates whether keycode is a layer tap key
-bool is_layer_tap(uint16_t keycode) {
-  return keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX;
-}
-
 // Tapping term per key
 uint16_t get_tapping_term(uint16_t keycode) {
-  if (is_layer_tap(keycode)) {
-    return TAPPING_TERM_LT;
-  } else {
-    return TAPPING_TERM;
+  switch (keycode) {
+    case LR_SPC:
+      return TAPPING_TERM_LONG;
+    default:
+      return TAPPING_TERM;
   }
 }
 
