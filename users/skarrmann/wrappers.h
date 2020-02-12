c58510291497cfa50907497beba00e7076024e51
Steven Karrmann <skarrmann@gmail.com>
2020-02-11 22:21:20 -0500

    Fix misplaced 'k' key in QWERTY base definition.

--- users/skarrmann/wrappers.h
+++ users/skarrmann/wrappers.h
@@ -8,7 +8,7 @@
 #define _________________QWERTY_L3_________________ KC_Z   , KC_X   , KC_C   , KC_V   , KC_B
 
 #define _________________QWERTY_R1_________________ KC_Y   , KC_U   , KC_I   , KC_O   , KC_P
-#define _________________QWERTY_R2_________________ KC_H   , KC_J   , KC_L   , KC_L   , KC_SCLN
+#define _________________QWERTY_R2_________________ KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN
 #define _________________QWERTY_R3_________________ KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH
 
 #define _________________COLEMAK_L1________________ KC_Q   , KC_W   , KC_F   , KC_P   , KC_G
