985b0645068f7a7901710567f14d743f471d6c20
Steven Karrmann <skarrmann@gmail.com>
2020-03-02 23:08:24 -0500

    Implement scaffolding for Kyria layout.

---
+++ keyboards/kyria/keymaps/skarrmann/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X64
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+  #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+#endif
+
+// Handle Elite-C on keyboard slave side
+#define SPLIT_USB_DETECT
+#define NO_USB_STARTUP_CHECK
