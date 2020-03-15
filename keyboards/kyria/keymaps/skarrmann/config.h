09abe624f880e396ca31f226bd1143169297e0ba
Steven Karrmann <skarrmann@gmail.com>
2020-03-15 17:53:59 -0400

    Perform initial assignment of Kyria thumb keys. Minor configuration tweaks for Kyria.

--- keyboards/kyria/keymaps/skarrmann/config.h
+++ keyboards/kyria/keymaps/skarrmann/config.h
@@ -1,16 +1,18 @@
 #pragma once
 
+#define EE_HANDS
+
 #ifdef OLED_DRIVER_ENABLE
   #define OLED_DISPLAY_128X64
 #endif
 
 #ifdef RGBLIGHT_ENABLE
   #define RGBLIGHT_ANIMATIONS
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_HUE_STEP 16
+  #define RGBLIGHT_SAT_STEP 16
+  #define RGBLIGHT_VAL_STEP 32
 #endif
 
-// Handle Elite-C on keyboard slave side
-#define SPLIT_USB_DETECT
-#define NO_USB_STARTUP_CHECK
+// Handle Elite C rev3 on the slave side
+// #define SPLIT_USB_DETECT
+// #define NO_USB_STARTUP_CHECK
