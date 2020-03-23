af278d6ccc72489ce1503e5502362507ebaced36
Steven Karrmann <skarrmann@gmail.com>
2020-03-22 23:51:03 -0400

    Clean up OLED status rendering.

--- keyboards/kyria/keymaps/skarrmann/keymap.c
+++ keyboards/kyria/keymaps/skarrmann/keymap.c
@@ -85,30 +85,24 @@ static void render_kyria_logo(void) {
     oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
 }
 
-static void render_qmk_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-      0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-      0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-      0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
-    };
-
-    oled_write_P(qmk_logo, false);
-}
-
-static void render_status(void) {
-    // QMK Logo and version information
-    render_qmk_logo();
-    oled_write_P(PSTR("skarrmann keymap\n\n"), false);
-
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-    switch (get_highest_layer(layer_state)) {
+void render_base_layer_status(void) {
+    oled_write_P(PSTR("Base: "), false);
+    switch (get_highest_layer(default_layer_state)) {
         case _QWERTY:
-            oled_write_P(PSTR("Default\n"), false);
+            oled_write_P(PSTR("Qwerty\n"), false);
             break;
         case _COLEMAK:
             oled_write_P(PSTR("Colemak\n"), false);
-            break;
+	    break;
+	default:
+            oled_write_P(PSTR("\n"), false);
+	    break;
+    }
+}
+
+void render_custom_layer_status(void) {
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
         case _SYMBOL:
             oled_write_P(PSTR("Symbol\n"), false);
             break;
@@ -120,12 +114,14 @@ static void render_status(void) {
             break;
 	case _FUNCTION:
             oled_write_P(PSTR("Function\n"), false);
-            break;
-        default:
-            oled_write_P(PSTR("Undefined\n"), false);
+	    break;
+	default:
+            oled_write_P(PSTR("\n"), false);
+	    break;
     }
+}
 
-    // Host Keyboard Modifiers
+void render_mod_status(void) {
     uint8_t mod_state = get_mods();
     oled_write_P(PSTR("Mod: "), false);
     oled_write_P((mod_state & MOD_MASK_SHIFT) ? PSTR("SFT ") : PSTR("    "), false);
@@ -133,12 +129,21 @@ static void render_status(void) {
     oled_write_P((mod_state & MOD_MASK_ALT)   ? PSTR("ALT ") : PSTR("    "), false);
     oled_write_P((mod_state & MOD_MASK_GUI)   ? PSTR("OS")   : PSTR("  ")  , false);
     oled_write_P(PSTR("\n"), false);
+}
 
-    // Host Keyboard LED Status
+void render_lock_status(void) {
     uint8_t led_usb_state = host_keyboard_leds();
     oled_write_P(IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK)    ? PSTR("NUMLCK ") : PSTR("       "), false);
     oled_write_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK)   ? PSTR("CAPLCK ") : PSTR("       "), false);
     oled_write_P(IS_LED_ON(led_usb_state, USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK ") : PSTR("       "), false);
+}
+
+void render_status(void) {
+    oled_write_P(PSTR("Keymap: skarrmann\n\n"), false);
+    render_base_layer_status();
+    render_custom_layer_status();
+    render_mod_status();
+    render_lock_status();
 }
 
 void oled_task_user(void) {
