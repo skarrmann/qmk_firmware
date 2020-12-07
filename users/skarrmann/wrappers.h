5ed165b82da8d48b62ebfa5bd38a46f45819fe49
Steven Karrmann <skarrmann@gmail.com>
2020-12-06 22:58:00 -0500

    Use familiar symbol assignment.

--- users/skarrmann/wrappers.h
+++ users/skarrmann/wrappers.h
@@ -2,7 +2,7 @@
 
 /* Base modifiers - for productivity and efficiency
  .-----------------------------------------------------. .-----------------------------------------------------.
- |  DEL   |        |        |        |        |        | |        |        |        |        |        |  BSPC  |
+ |   `    |        |        |        |        |        | |        |        |        |        |        |   -    |
  |        |        |        |        |        |        | |        |        |        |        |        |        |
  |--------+--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------+--------|
  |        |        |        |        |        |        | |        |        |        |        |        |   '    |
@@ -11,15 +11,15 @@
  |  ESC   |        |        |        |        |        | |        |        |        |        |        |  ENT   |
  |        |        |        |        |        |        | |        |        |        |        |        |        |
  |--------+--------+--------+--------+--------+--------| |--------+--------+--------+--------+--------+--------|
- |        |        |        |        |        |  TAB   | |  SPC   |        |        |        |        |        |
+ |        |        |        |        |  DEL   |  TAB   | |  SPC   |  BSPC  |        |        |        |        |
  |        |        |  LGUI  |  LALT  |  LCTL  | SYMBOL | |FUNCTION|  RCTL  |  RALT  |  RGUI  |        |        |
  '-----------------------------------------------------' '-----------------------------------------------------'
  */
-#define B_L1 KC_DEL
+#define B_L1 KC_GRV
 #define B_L2 KC_LSFT
 #define B_L3 KC_ESC
 
-#define B_R1 KC_BSPC
+#define B_R1 KC_MINS
 #define B_R2 MT(MOD_RSFT, KC_QUOT)
 #define B_R3 KC_ENT
 
@@ -30,10 +30,10 @@
 #define B_B1 _______
 #define B_B2 KC_LGUI
 #define B_B3 KC_LALT
-#define B_B4 KC_LCTL
+#define B_B4 MT(MOD_LCTL, KC_DEL)
 #define B_B5 LT(_SYMBOL, KC_TAB)
 #define B_B6 LT(_FUNCTION, KC_SPC)
-#define B_B7 KC_RCTL
+#define B_B7 MT(MOD_RCTL, KC_BSPC)
 #define B_B8 KC_RALT
 #define B_B9 KC_RGUI
 #define B_BA _______
@@ -62,15 +62,15 @@
 /* Custom layers */ 
 
 #define SYMBOL_L0___________________________________________ _______, _______, _______, _______, _______, _______
-#define SYMBOL_L1___________________________________________ KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   
-#define SYMBOL_L2___________________________________________ _______, KC_EXLM, KC_LPRN, KC_RPRN, KC_DLR , KC_AMPR
+#define SYMBOL_L1___________________________________________ KC_TILD, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   
+#define SYMBOL_L2___________________________________________ KC_HASH, KC_EXLM, KC_LPRN, KC_RPRN, KC_DLR , KC_AMPR
 #define SYMBOL_L3___________________________________________ _______, KC_AT  , KC_LBRC, KC_RBRC, KC_PLUS, KC_PERC
 #define SYMBOL_L4___________________________________________ _______, _______, _______, _______, _______, _______
 
 #define SYMBOL_R0___________________________________________ _______, _______, _______, _______, _______, _______
-#define SYMBOL_R1___________________________________________ KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_TILD
-#define SYMBOL_R2___________________________________________ KC_PIPE, KC_EQL , KC_MINS, KC_UNDS, KC_HASH, _______
-#define SYMBOL_R3___________________________________________ KC_CIRC, KC_ASTR, KC_LCBR, KC_RCBR, KC_BSLS, _______
+#define SYMBOL_R1___________________________________________ KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_UNDS
+#define SYMBOL_R2___________________________________________ KC_PIPE, KC_EQL , KC_LCBR, KC_RCBR, KC_COLN, KC_DQUO
+#define SYMBOL_R3___________________________________________ KC_CIRC, KC_ASTR, KC_LABK, KC_RABK, KC_QUES, KC_BSLS
 #define SYMBOL_R4___________________________________________ KC_SPC , _______, _______, _______, _______, _______
 
 #define FUNCTION_L0_________________________________________ _______, _______, _______, _______, _______, _______
