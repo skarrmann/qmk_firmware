f70e071f65164410a06ff65534054fa7eb148c9b
Steven Karrmann <skarrmann@gmail.com>
2021-06-27 02:11:05 -0400

    Define left/right base wrapper macros for concat layout

--- keyboards/concat/keymaps/skarrmann/keymap.c
+++ keyboards/concat/keymaps/skarrmann/keymap.c
@@ -18,36 +18,57 @@
 
 #define LAYOUT_concat_wrapper(...) LAYOUT(__VA_ARGS__)
 
+#define LAYOUT_concat_base_left( \
+  K11, K12, K13, K14, K15, \
+  K21, K22, K23, K24, K25, \
+  K31, K32, K33, K34, K35  \
+) \
+LAYOUT_concat_wrapper( \
+           _______, NUMROW_L0__________________________________,          \
+           B_LO1  , K11    , K12    , K13    , K14    , K15    ,          \
+           B_LO2  , B_LH_MODS(K21, K22, K23, K24)     , K25    ,          \
+  _______, B_LO3  , K31    , K32    , K33    , K34    , K35    , B_LI3  , \
+  _______, _______, _______,                   B_LB2  , B_LB1  , B_LI4    \
+)
+
+#define LAYOUT_concat_base_left_wrapper(...) LAYOUT_concat_base_left(__VA_ARGS__)
+
+#define LAYOUT_concat_base_right( \
+  K11, K12, K13, K14, K15, \
+  K21, K22, K23, K24, K25, \
+  K31, K32, K33, K34, K35  \
+) \
+LAYOUT_concat_wrapper( \
+           _______, NUMROW_R0__________________________________,          \
+           K11    , K12    , K13    , K14    , K15    , B_RO1  ,          \
+           K21    , B_LH_MODS(K22, K23, K24, K25)     , B_RO2  ,          \
+  B_RI3  , K31    , K32    , K33    , K34    , K35    , B_RO3  , _______, \
+  B_RI4  , B_RB1  , B_RB2  ,                   _______, _______, _______  \
+)
+
+#define LAYOUT_concat_base_right_wrapper(...) LAYOUT_concat_base_right(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [_QWERTY] = {
-        LAYOUT_concat_wrapper( \
-                 _______, NUMROW_L0__________________________________,          \
-                 B_LO1  , QWERTY_L1__________________________________,          \
-                 B_LO2  , QWERTY_L2__________________________________,          \
-        _______, B_LO3  , QWERTY_L3__________________________________, B_LI3  , \
-        _______, _______, _______,                   B_LB2  , B_LB1  , B_LI4),
-        LAYOUT_concat_wrapper( \
-                 NUMROW_R0__________________________________, _______,          \
-                 QWERTY_R1__________________________________, B_RO1  ,          \
-                 QWERTY_R2__________________________________, B_RO2  ,          \
-        B_RI3  , QWERTY_R3__________________________________, B_RO3  , _______, \
-        B_RI4  , B_RB1  , B_RB2  ,                   _______, _______, _______)
+        LAYOUT_concat_base_left_wrapper( \
+            QWERTY_L1__________________________________, \
+            QWERTY_L2__________________________________, \
+            QWERTY_L3__________________________________),
+        LAYOUT_concat_base_right_wrapper( \
+            QWERTY_R1__________________________________, \
+            QWERTY_R2__________________________________, \
+            QWERTY_R3__________________________________)
     },
     [_COLEMAK] = {
-        LAYOUT_concat_wrapper( \
-                 _______, NUMROW_L0__________________________________,          \
-                 B_LO1  , COLEMAK_L1_________________________________,          \
-                 B_LO2  , COLEMAK_L2_________________________________,          \
-        _______, B_LO3  , COLEMAK_L3_________________________________, _______, \
-        _______, _______, _______,                   B_LB2  , B_LB1  , B_LI4),
-        LAYOUT_concat_wrapper( \
-                 NUMROW_R0__________________________________, _______,          \
-                 COLEMAK_R1_________________________________, B_RO1  ,          \
-                 COLEMAK_R2_________________________________, B_RO2  ,          \
-        B_RI3  , COLEMAK_R3_________________________________, B_RO3  , _______, \
-        B_RI4  , B_RB1  , B_RB2  ,                   _______, _______, _______)
+        LAYOUT_concat_base_left_wrapper( \
+            COLEMAK_L1_________________________________, \
+            COLEMAK_L2_________________________________, \
+            COLEMAK_L3_________________________________),
+        LAYOUT_concat_base_right_wrapper( \
+            COLEMAK_R1_________________________________, \
+            COLEMAK_R2_________________________________, \
+            COLEMAK_R3_________________________________)
     },
     [_SYMBOL] = {
 	LAYOUT_concat_wrapper( \
