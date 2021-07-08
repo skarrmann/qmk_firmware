90b10783db2a5f83865c1b0b411467b9a128f79e
skarrmann <skarrmann@gmail.com>
2021-07-08 00:34:50 +0000

    Implement distinct side mods with OSM. Simplify SYM layer

--- users/skarrmann/skarrmann.c
+++ users/skarrmann/skarrmann.c
@@ -1,16 +1,16 @@
 #include "skarrmann.h"
 
 // Custom key names
-#define FUN_DEL LT(FUN, KC_DEL)
 #define SYM_TAB LT(SYM, KC_TAB)
-#define NAV_SPC LT(NAV, KC_SPC)
+#define FUN_SPC LT(FUN, KC_SPC)
 
-#define ALT_MIN RALT_T(KC_MINS)
-#define SFT_QUO RSFT_T(KC_QUOT)
+#define OSM_SFT OSM(MOD_LSFT)
+#define OSM_CTL OSM(MOD_LCTL)
+#define OSM_ALT OSM(MOD_LALT)
+#define OSM_GUI OSM(MOD_LGUI)
 
 #define TG_SYM  TG(SYM)
 #define TG_FUN  TG(FUN)
-#define TG_NAV  TG(NAV)
 #define TG_GAM  TG(GAM)
 #define TG_HOM  TG(HOM)
 
@@ -27,10 +27,10 @@
 // Layer key mappings
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [DEF] = LAYOUT_skarrmann(
-    KC_LALT, KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   ,                   KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, ALT_MIN,
-    KC_LSFT, KC_A   , KC_R   , KC_S   , KC_T   , KC_D   ,                   KC_H   , KC_N   , KC_E   , KC_I   , KC_O   , SFT_QUO,
-    KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RCTL,
-                                        FUN_DEL, SYM_TAB, KC_ESC , KC_ENT , NAV_SPC, KC_BSPC
+    KC_ESC , KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   ,                   KC_J   , KC_L   , KC_U   , KC_Y   , KC_QUOT, KC_DEL ,
+    OSM_SFT, KC_A   , KC_R   , KC_S   , KC_T   , KC_D   ,                   KC_H   , KC_N   , KC_E   , KC_I   , KC_O   , OSM_GUI,
+    OSM_CTL, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, OSM_ALT,
+                                        KC_DEL , SYM_TAB, KC_ESC , KC_ENT , FUN_SPC, KC_BSPC
   ),
 
   [HOM] = LAYOUT_skarrmann(
@@ -41,23 +41,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [SYM] = LAYOUT_skarrmann(
-    KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_UNDS,
-    KC_HASH, KC_EXLM, KC_LPRN, KC_RPRN, KC_DLR , KC_AMPR,                   KC_PIPE, KC_EQL , KC_LCBR, KC_RCBR, KC_COLN, KC_DQUO,
-    KC_TILD, KC_AT  , KC_LBRC, KC_RBRC, KC_PLUS, KC_PERC,                   KC_CIRC, KC_ASTR, KC_LABK, KC_RABK, KC_QUES, KC_BSLS,
+    _______, _______, KC_GRV , KC_MINS, KC_EQL , _______,                   _______, KC_BSLS, KC_BSLS, KC_COLN, KC_QUOT, _______,
+    _______, KC_1   , KC_2   , KC_3   , KC_4   , _______,                   _______, KC_7   , KC_8   , KC_9   , KC_0   , _______,
+    _______, _______, KC_LBRC, KC_RBRC, KC_5   , _______,                   _______, KC_6   , KC_COMM, KC_DOT , KC_SLSH, _______,
                                         _______, _______, _______, _______, KC_SPC , _______
   ),
 
-  [NAV] = LAYOUT_skarrmann(
-    _______, _______, _______, _______, _______, _______,                   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______,
-    _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,                   _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______,
-    _______, _______, _______, _______, _______, _______,                   _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_APP , _______,
-                                        _______, KC_TAB , _______, _______, _______, _______
-  ),
-
   [FUN] = LAYOUT_skarrmann(
-    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_PSCR,                   TG_HOM , _______, _______, _______, _______, _______,
-    _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_INS ,                   EEP_RST, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, _______,
-    _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_CAPS,                   TG_GAM , _______, _______, _______, _______, _______,
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_PSCR,                   TG_HOM , KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______,
+    _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_INS ,                   EEP_RST, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______,
+    _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_CAPS,                   TG_GAM , KC_MUTE, KC_VOLD, KC_VOLU, KC_APP , _______,
                                         _______, _______, _______, _______, _______, _______
   ),
 
