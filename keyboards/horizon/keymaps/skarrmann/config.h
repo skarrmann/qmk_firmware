9e9f7872d82055fe78f3a80ed60e123bb951a340
skarrmann <skarrmann@gmail.com>
2021-10-09 13:56:01 +0000

    Fix Horizon pins

--- keyboards/horizon/keymaps/skarrmann/config.h
+++ keyboards/horizon/keymaps/skarrmann/config.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#ifdef OLED_DRIVER_ENABLE
-  #define OLED_DISPLAY_128X64
-#endif
-
 #define LAYOUT_skarrmann(                                                                                                        \
     K00    , K01    , K02    , K03    , K04    , K05    ,                   K06    , K07    , K08    , K09    , K0A    , K0B    ,\
     K10    , K11    , K12    , K13    , K14    , K15    ,                   K16    , K17    , K18    , K19    , K1A    , K1B    ,\
