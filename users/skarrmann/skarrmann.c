f64372504ce5224d4ee75a18d428b52bf85b4920
Steven Karrmann <skarrmann@gmail.com>
2021-10-17 20:43:48 -0400

    Remove encoder assignment from skarrmann userspace

--- users/skarrmann/skarrmann.c
+++ users/skarrmann/skarrmann.c
@@ -58,26 +58,3 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             return TAPPING_TERM;
     }
 }
-
-
-#ifdef ENCODER_ENABLE
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        // Volume control
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
-    }
-    else if (index == 1) {
-        // Page up/Page down
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
-    return true;
-}
-#endif
