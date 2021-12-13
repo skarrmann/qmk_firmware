64c32ac566699c83ef2947321aee074a8672d7a0
Steven Karrmann <skarrmann@gmail.com>
2021-12-12 20:07:43 -0500

    Define initial ChocV keymap (pending combo + layer tap)

--- keyboards/chocv/keymaps/default/keymap.c
+++ keyboards/chocv/keymaps/default/keymap.c
@@ -27,21 +27,21 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,  KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   ,
-        KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,  KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN,
-        KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,
+        KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   ,  KC_J   , KC_L   , KC_U   , KC_Y   , KC_QUOT,
+        KC_A   , KC_R   , KC_S   , KC_T   , KC_D   ,  KC_H   , KC_N   , KC_E   , KC_I   , KC_O,
+        KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,  KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,
                           KC_LALT, KC_LCTL, KC_LSFT,  KC_SPC, MO_SYMB, MO_FUNC
     ),
     [_SYMBOL] = LAYOUT(
         KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,
-        KC_EXLM, KC_LPRN, KC_RPRN, KC_DLR , KC_AMPR, KC_PIPE, KC_HASH, KC_LCBR, KC_RCBR, KC_COLN,
-        KC_AT  , KC_LBRC, KC_RBRC, KC_BSLS, KC_PERC, KC_CIRC, KC_ASTR, KC_LABK, KC_RABK, KC_QUES,
-                         _______, _______, _______, _______, _______, _______
+        KC_GRV , KC_SCLN, KC_EQL , KC_MINS, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT,
+        _______, KC_LBRC, KC_RBRC, KC_BSLS, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END ,
+                          _______, _______, _______, _______, _______, _______
     ),
     [_FUNCTION] = LAYOUT(
-        KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_PSCR, KC_SLCK, KC_HOME, KC_PGDN, KC_PGUP, KC_END ,
-        KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_INS , KC_CLCK, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT,
-        KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_PAUS,
+        KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, _______, KC_CAPS, KC_SLCK, KC_NLCK, _______,
+        KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, _______, KC_INS , KC_PSCR, KC_PAUS, _______,
+        KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
                           _______, _______, _______, _______, _______, _______
     )
 };
