510a667632bad85d206cd4e5fd551dbc9aa48d49
Steven Karrmann <skarrmann@gmail.com>
2022-05-13 17:27:07 -0400

    Use conventional steno * position

--- users/skarrmann/skarrmann.c
+++ users/skarrmann/skarrmann.c
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [STN] = LAYOUT_skarrmann(
-        STN_FN  , STN_S1 , STN_TL , STN_PL , STN_HL , STN_N4 ,                   STN_N8 , STN_FR , STN_PR , STN_LR , STN_TR , STN_DR  ,
-        STN_PWR , STN_S2 , STN_KL , STN_WL , STN_RL , STN_ST2,                   STN_ST3, STN_RR , STN_BR , STN_GR , STN_SR , STN_ZR  ,
-        STN_RES1, STN_N1 , STN_N2 , STN_N3 , STN_ST1, STN_N5 , _______, _______, STN_N9 , STN_ST4, STN_NA , STN_NB , STN_NC , STN_RES2,
-        _______ , _______, _______, _______, STN_A  , STN_O  , STN_N6 , STN_N7 , STN_E  , STN_U  , _______, _______, _______, _______
+        STN_FN  , STN_S1 , STN_TL , STN_PL , STN_HL, STN_ST1,                   STN_ST3, STN_FR , STN_PR , STN_LR , STN_TR , STN_DR  ,
+        STN_PWR , STN_S2 , STN_KL , STN_WL , STN_RL, STN_ST2,                   STN_ST4, STN_RR , STN_BR , STN_GR , STN_SR , STN_ZR  ,
+        STN_RES1, STN_N1 , STN_N2 , STN_N3 , STN_N4, STN_N5 , _______, _______, STN_N8 , STN_N9 , STN_NA , STN_NB , STN_NC , STN_RES2,
+        _______ , _______, _______, _______, STN_A , STN_O  , STN_N6 , STN_N7 , STN_E  , STN_U  , _______, _______, _______, _______
     ),
 };
 
