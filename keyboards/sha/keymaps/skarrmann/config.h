66f9a59d1db0d168652cb4a2bcbcc4999865af72
skarrmann <skarrmann@gmail.com>
2023-12-24 12:00:26 -0500

    Enable RGB Lighting Layers in Sha skarrmann keymap

--- keyboards/sha/keymaps/skarrmann/config.h
+++ keyboards/sha/keymaps/skarrmann/config.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define RGBLIGHT_LAYERS
+
 #define LAYOUT_skarrmann( \
     K00    , K01    , K02    , K03    , K04    , K05    ,                   K06    , K07    , K08    , K09    , K0A    , K0B    , \
     K10    , K11    , K12    , K13    , K14    , K15    ,                   K16    , K17    , K18    , K19    , K1A    , K1B    , \
